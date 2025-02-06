2f5ea30168db775258c1eff60347622eebd0eace
thedashpuntsag <thedashpuntsag@gmail.com>
2025-02-06 19:45:16 +0800

    adding queryParams parser

---
+++ packages/global-dynamo/utils/query-parsing.ts
@@ -0,0 +1,32 @@
+import type { ValidatedAPIGatewayProxyEvent } from '@custom-repo/global-libs';
+import type { QueryRequest } from '../types';
+
+import { QueryRequestSchema } from '../types';
+import { CustomError } from '@custom-repo/global-libs';
+
+type EventType = ValidatedAPIGatewayProxyEvent<object | null>;
+
+export function extractQueryParamsFromEvent(event: EventType, indexName: string, query: QueryRequest): QueryRequest {
+  let parsedQueries = event.queryStringParameters || {};
+
+  if (parsedQueries && parsedQueries.index === indexName) {
+    parsedQueries = {
+      indexName,
+      pKey: parsedQueries.pKey || query.pKey,
+      pKeyType: parsedQueries.pKeyType || query.pKeyType,
+      pKeyProp: parsedQueries.pKeyProps || query.pKeyProp
+    };
+  }
+
+  const parseResult = QueryRequestSchema.safeParse({
+    indexName: parsedQueries.index,
+    ...parsedQueries,
+    limit: parsedQueries.limit || '10'
+  });
+
+  if (!parseResult.success) {
+    throw new CustomError(`Bad request!,${parseResult.error.errors.map((err) => err.path).join(', ')}`, 400);
+  }
+
+  return parseResult.data;
+}
