7f93d04cc5b3922c88d48818a652fbcaf56bfc92
thedashpuntsag <thedashpuntsag@gmail.com>
2025-08-20 16:43:34 +0800

    fix: updated example stacks serverless.ts

--- stacks/example-stack/serverless.ts
+++ stacks/example-stack/serverless.ts
@@ -15,10 +15,31 @@ const serverlessConfig: AWS = {
   provider: {
     name: 'aws',
     stage: "${opt:stage, 'prod'}",
-    runtime: 'nodejs18.x',
+    runtime: 'nodejs20.x',
     region: 'ap-southeast-1',
     profile: 'default',
-    logRetentionInDays: 365
+    timeout: 29,
+    memorySize: 512,
+    architecture: 'arm64',
+    deploymentBucket: {
+      blockPublicAccess: true
+    },
+    apiGateway: {
+      minimumCompressionSize: 1024, // Compress responses larger than 1KB
+      shouldStartNameWithService: true, // Include the service name in API Gateway endpoint URLs
+      usagePlan: {
+        throttle: {
+          burstLimit: 150, // Maximum number of requests per second
+          rateLimit: 100 // Average number of requests per second
+        }
+      }
+    },
+    logRetentionInDays: 365,
+    environment: {
+      STAGE: '${self:provider.stage}',
+      REGION: '${self:provider.region}'
+    },
+    iam: { role: 'YOUR_ROLE_HERE' }
   },
   functions: {
     getExampleTableDesc,
@@ -28,25 +49,7 @@ const serverlessConfig: AWS = {
     putUpdateExampleItem
   },
   package: { individually: true },
-  custom: {
-    prune: {
-      automatic: true,
-      number: 2
-    },
-    function_timeout: {
-      ain: 29
-    },
-    esbuild: {
-      bundle: true,
-      minify: false,
-      sourcemap: true,
-      exclude: ['aws-sdk', 'pg-hstore'],
-      target: 'node18',
-      define: { 'require.resolve': undefined },
-      platform: 'node',
-      concurrency: 10
-    }
-  }
+  custom: { prune: { automatic: true, number: 2 } }
 };
 
-module.exports = serverlessConfig;
+export default serverlessConfig;
